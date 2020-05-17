fb493f7d35b5bb4873a825303d846b3fc2f47fb3
Tomas Rosenqvist <tomas.granlund@mensa.se>
2020-05-17 10:33:56 +0200

    :white_check_mark: ArgumentHelpersTests

---
+++ Acme.Tests/ArgumentHelpersTests.cs
@@ -0,0 +1,32 @@
+﻿using Acme.Graphs;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Acme.Tests {
+    public class ArgumentHelpersTests {
+        [Fact]
+        public void DoesNotThrowIfExpressionIsNotNull() {
+            typeof(ArgumentHelpers).Invoking(_ => 
+                ArgumentHelpers.ThrowIfNull(() => "not null"))
+                .Should().NotThrow();
+        }
+
+        [Fact]
+        public void ThrowsIfExpressionIsNull() {
+            typeof(ArgumentHelpers).Invoking(_ =>
+                ArgumentHelpers.ThrowIfNull<object>(() => null))
+                .Should().Throw<ArgumentNullException>()
+                .Which.ParamName.Should().Be("() => null");
+        }
+
+        [Fact]
+        public void IndicatesParameterByNameIfNull() {
+            string nullParam = null;
+            typeof(ArgumentHelpers).Invoking(_ =>
+                ArgumentHelpers.ThrowIfNull(() => nullParam))
+                .Should().Throw<ArgumentNullException>()
+                .Which.ParamName.Should().Be(nameof(nullParam));
+        }
+    }
+}
