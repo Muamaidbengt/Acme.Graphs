2c0430bd8fffa8def5eb0fd177c4a9bbfa732942
Tomas Rosenqvist <tomas.granlund@mensa.se>
2020-05-17 10:34:51 +0200

    :sparkles: adds Path

---
+++ Acme.Graphs/Path.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Acme.Graphs {
+    public class Path : IEnumerable<NodeIdentity> {
+        private readonly List<NodeIdentity> _steps;
+        private Path(IEnumerable<NodeIdentity> steps) {
+            ArgumentHelpers.ThrowIfNull(() => steps);
+            _steps = new List<NodeIdentity>(steps);
+        }
+
+        public static Path Of(IEnumerable<NodeIdentity> steps) {
+            return new Path(steps);
+        }
+
+        public static Path Of(params NodeIdentity[] steps) {
+            return new Path(steps);
+        }
+
+        public static Path Empty { get => new Path(new List<NodeIdentity>()); }
+
+        public IEnumerator<NodeIdentity> GetEnumerator() => _steps.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Acme.Tests/PathTests.cs
@@ -0,0 +1,52 @@
+﻿using Acme.Graphs;
+using FluentAssertions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Acme.Tests {
+    public class PathTests {
+        [Fact]
+        public void CanBeEnumerated() {
+            typeof(Path).Should().Implement<IEnumerable<NodeIdentity>>();
+        }
+
+        [Fact]
+        public void StepsMustNotBeNull() {
+            typeof(Path).Invoking(_ => Path.Of(null))
+                .Should().Throw<ArgumentNullException>()
+                .Which.ParamName.Should().Be("steps");
+        }
+
+        [Fact]
+        public void EmptyPathIsEmpty() {
+            Path.Empty.Should().BeEmpty();
+
+            ((IEnumerable)Path.Empty).GetEnumerator()
+                .MoveNext().Should().BeFalse();
+        }
+
+        [Fact]
+        public void NonEmptyPathIsNotEmpty() {
+            Path.Of(NodeIdentity.Of("foo")).Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void CannotBeMutated() {
+            var nodes = new List<NodeIdentity> { NodeIdentity.Of("foo") };
+            var path = Path.Of(nodes);
+            nodes.Add(NodeIdentity.Of("bar"));
+            
+            path.Should().NotContain(NodeIdentity.Of("bar"));
+        }
+
+        [Fact]
+        public void StepOrderIsRetained() {
+            var path = Path.Of(NodeIdentity.Of("first"), NodeIdentity.Of("second"));
+            path.Should().ContainInOrder(NodeIdentity.Of("first"), NodeIdentity.Of("second"));
+        }
+
+        // TODO: equality tests
+    }
+}
