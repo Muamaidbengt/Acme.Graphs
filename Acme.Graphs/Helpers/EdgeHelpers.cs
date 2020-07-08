821d4cf89b3633a7b0041c3afbef60bb6adfce20
Tomas Rosenqvist <tomas.granlund@mensa.se>
2020-07-08 14:43:52 +0200

    :sparkles: helpers checking if edge goes in/out of path

---
+++ Acme.Graphs/Helpers/EdgeHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Acme.Graphs.Helpers {
+    public static class EdgeHelpers {
+        public static bool LeadsInto(this DirectedEdge edge, Path path) {
+            return path.Any(n => edge.To == n) && path.All(n => edge.From != n);
+        }
+
+        public static bool LeadsOutOf(this DirectedEdge edge, Path path) {
+            return path.All(n => edge.To != n) && path.Any(n => edge.From == n);
+        }
+    }
+}
---
+++ Acme.Tests/EdgeHelpersTests.cs
@@ -0,0 +1,35 @@
+﻿using Acme.Graphs.Helpers;
+using Acme.Tests.TestHelpers;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace Acme.Tests {
+    public class EdgeHelpersTests {
+        [Theory]
+        [InlineData("ABC", "D-A", true)]
+        [InlineData("ABC", "D-B", true)]
+        [InlineData("ABC", "D-E", false)]
+        [InlineData("ABC", "A-B", false)]
+        [InlineData("ABC", "C-A", false)]
+        public void LeadsInto(string pathDescription, string edgeDescription, bool expected) {
+            var path = GraphFactory.CreatePath(pathDescription.Select(c => c.ToString()).ToArray());
+            var edge = GraphFactory.CreateEdge(edgeDescription);
+
+            edge.LeadsInto(path).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("ABC", "A-D", true)]
+        [InlineData("ABC", "B-D", true)]
+        [InlineData("ABC", "D-E", false)]
+        [InlineData("ABC", "A-B", false)]
+        [InlineData("ABC", "C-A", false)]
+        public void LeadsOutOf(string pathDescription, string edgeDescription, bool expected) {
+            var path = GraphFactory.CreatePath(pathDescription.Select(c => c.ToString()).ToArray());
+            var edge = GraphFactory.CreateEdge(edgeDescription);
+
+            edge.LeadsOutOf(path).Should().Be(expected);
+        }
+    }
+}
