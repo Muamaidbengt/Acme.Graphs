ead22d3d180b475ea85c9400154cf44118f31a40
Tomas Rosenqvist <tomas.granlund@mensa.se>
2020-07-08 14:42:02 +0200

    :sparkles: helper class for tracking object instances

---
+++ Acme.Graphs/Helpers/ObjectReferenceHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Acme.Graphs.Helpers {
+    public static class ObjectReferenceHelpers {
+        private static readonly ConditionalWeakTable<object, RefId> _ids = new ConditionalWeakTable<object, RefId>();
+
+        public static Guid GetRefId<T>(this T obj) where T : class {
+            if (ReferenceEquals(obj, null)) {
+                return default;
+            }
+
+            return _ids.GetOrCreateValue(obj).Id;
+        }
+
+        private class RefId {
+            public Guid Id { get; } = Guid.NewGuid();
+        }
+    }
+}
---
+++ Acme.Tests/ObjectReferenceHelpersTests.cs
@@ -0,0 +1,41 @@
+﻿using Acme.Graphs;
+using Acme.Graphs.Helpers;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Acme.Tests {
+    public class ObjectReferenceHelpersTests {
+        [Fact]
+        public void InstanceHasSameRefId() {
+            var p1 = NodeIdentity.Of("test");
+            var firstRefId = p1.GetRefId();
+            var secondRefId = p1.GetRefId();
+
+            secondRefId.Should().Be(firstRefId);
+        }
+
+        [Fact]
+        public void TwoPointersToSameInstanceHasSameRefId() {
+            var p1 = NodeIdentity.Of("test");
+            var p2 = p1;
+
+            p2.GetRefId().Should().Be(p1.GetRefId());
+        }
+
+        [Fact]
+        public void TwoInstancesWithSameValuesHaveDifferentRefId() {
+            var p1 = NodeIdentity.Of("test");
+            var p2 = NodeIdentity.Of("test");
+
+            p2.GetRefId().Should().NotBe(p1.GetRefId());
+        }
+
+        [Fact]
+        public void NullValueHasEmptyGuidRefId() {
+            var p1 = (NodeIdentity)null;
+
+            p1.GetRefId().Should().Be(Guid.Empty);
+        }
+    }
+}
