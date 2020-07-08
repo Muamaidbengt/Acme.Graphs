5c964b2351fb13d0ee5939d0ca167c8ed07d106f
Tomas Rosenqvist <tomas.granlund@mensa.se>
2020-07-08 20:39:37 +0200

    adds test cases

--- Acme.Tests/CycleReducerTests.cs
+++ Acme.Tests/CycleReducerTests.cs
@@ -6,6 +6,22 @@ using Xunit;
 namespace Acme.Tests {
     public class CycleReducerTests {
         [Fact]
+        public void GraphWithoutCyclesRemainsUntouched() {
+            var graph = GraphFactory.BuildGraph("0-1", "1-2", "2-3", "2-4");
+            var acyclic = CycleReducer.RemoveCycles(graph);
+            acyclic.Cycles.Should().HaveCount(0);
+            acyclic.GraphWithoutCycles.Nodes.Should().HaveCount(5);
+        }
+
+        [Fact]
+        public void GraphWithOnlyACycleIsSubstituted() {
+            var graph = GraphFactory.BuildGraph("0-1", "1-2", "2-0");
+            var acyclic = CycleReducer.RemoveCycles(graph);
+            acyclic.Cycles.Should().HaveCount(1);
+            acyclic.GraphWithoutCycles.Nodes.Should().HaveCount(1);
+        }
+
+        [Fact]
         public void ReducedGraphsHaveCorrectNodeAndCycleCount() {
             var graph = GraphFactory.BuildGraph(
                 "0-1", "0-4",
